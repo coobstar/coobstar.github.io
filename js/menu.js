efb3d5b339651cfa50a027f1ba76ed0baf3a0c7e
coobstar <carl.majeau@gmail.com>
2018-10-12 18:14:38 -0400

    menu componentized

--- js/menu.js
+++ js/menu.js
@@ -1,38 +1,22 @@
+var Menu = function(items, id="menu") {
+  var menu = document.getElementById(id);
+  var menuButton = document.getElementById('menuButton');
 
+  let menuState = ["closed", "open"];
 
-var Menu = function(items) {
-  var menu = document.getElementById('menu');
-  var tag = document.getElementById('tag');
-
-  for (item of items) {
+  for (let item of items) {
     let menuItem = document.createElement("div");
     menuItem.id = item.name;
-    menuItem.addEventListener("click", item.callback);
+    menuItem.addEventListener("click", function(e) {
+      item.callback(e, menu, item);
+    });
+    menuItem.style.backgroundImage = "url('" + item.image + "')";
     menu.appendChild(menuItem);
   }
+
+  menuButton.addEventListener("click", function(e){
+    [menuState[0], menuState[1]] = [menuState[1], menuState[0]];
+    menu.classList.remove(menuState[1]);
+    menu.classList.add(menuState[0]);
+  });
 };
-
-var lights = document.getElementById('lights');
-var save = document.getElementById('save');
-
-var backgrounds = ["white", "black"];
-var images = ["img/moon.svg", "img/sun.svg"];
-var inverse = [0, 100];
-var menuState = ["closed", "open"];
-tag.addEventListener("click", function(e){
-  [menuState[0], menuState[1]] = [menuState[1], menuState[0]];
-  menu.classList.remove(menuState[1]);
-  menu.classList.add(menuState[0]);
-});
-lights.addEventListener("click", function(e){
-  [backgrounds[0], backgrounds[1]] = [backgrounds[1], backgrounds[0]];
-  [images[0], images[1]] = [images[1], images[0]];
-  [inverse[0], inverse[1]] = [inverse[1], inverse[0]];
-  document.body.style.backgroundColor = backgrounds[0];
-  lights.style.backgroundImage = "url('" + images[0] + "')";
-  menu.style.filter = "invert(" + inverse[0] + "%)";
-});
-save.addEventListener("click", function(e) {
-  var image = document.getElementById('defaultCanvas0').toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-  window.location.href=image;
-});
